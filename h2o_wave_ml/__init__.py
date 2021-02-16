b3f53fce02574bb7eb5bccf19318df531cca83fb
geomodular <peter.szabo@h2o.ai>
2021-02-16 18:01:02 +0100

    Update init file

--- h2o_wave_ml/__init__.py
+++ h2o_wave_ml/__init__.py
@@ -16,5 +16,4 @@
 Python package `h2o_wave_ml` provides the API functions for automatic machine learning tasks.
 """
 
-from .ml import (Model, ModelType, ModelMetric, DataSource,
-                 build_model, get_model, save_model, load_model)
+from .ml import Model, ModelType, ModelMetric, build_model, get_model, save_model, load_model
