c6f47bef18e387c5e4caa50b4febee44ff846053
geomodular <peter.szabo@h2o.ai>
2021-03-17 12:46:12 +0100

    chore: rewrite `_get_prediction_output()` #8 to be more efficient

--- h2o_wave_ml/ml.py
+++ h2o_wave_ml/ml.py
@@ -282,19 +282,14 @@ class _DAIModel(Model):
         return name.split('.')[-1]
 
     @classmethod
-    def _get_prediction_output(cls, f) -> List[Tuple]:
-        rows = f.to_tuples()
-        names = [cls._extract_class(name) for name in f.names]
+    def _get_prediction_output(cls, df: dt.frame) -> List[Tuple]:
+        names = [cls._extract_class(name) for name in df.names]
 
         ret = []
-        for row in rows:
-            max_value = row[0]
-            value_index = 0
-            for i, value in enumerate(row):
-                if value > max_value:
-                    max_value = value
-                    value_index = i
-            ret.append(tuple([names[value_index], *row]))
+        for i in range(df.nrows):
+            row = df[i, :].to_tuples()[0]
+            index = row.index(max(row))
+            ret.append(tuple([names[index], *row]))
 
         return ret
 
