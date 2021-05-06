428ce09e04eccb09f4539f05b01166d5b34474cc
geomodular <peter.szabo@h2o.ai>
2021-05-06 18:48:00 +0200

    feat: add cloud version of apps
    commit 71c53eaa3344f46dfbc5c57d4e96f1cb451dc2a7
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Thu May 6 18:45:41 2021 +0200
    
        chore: rename app directories
    
    commit e1697dc4866f83765489cc0a19ec9b26720211fd
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Wed May 5 12:32:56 2021 +0200
    
        chore: update `toml` files
    
    commit 2d2ee80b50405409257865c4c59bcccf9b177956
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Tue May 4 16:40:11 2021 +0200
    
        chore: clean up
    
    commit 50159ec31684ae09e40f4fa0df3b2a67b48a9c8a
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Fri Apr 30 16:15:38 2021 +0200
    
        feat: make wine app from wine example
    
    commit e1563920a5c5e8661da677c19abe449dbd53dbaf
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Fri Apr 23 16:22:35 2021 +0200
    
        feat: make example app for cloud
    
    commit b61ebf76ab86f29b6c7a2e5eef3faef68b7694ee
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Thu Apr 22 18:23:22 2021 +0200
    
        chore: clean up
    
    commit 433371107eb80b830d4d1233eac73ecc720c16b6
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Thu Apr 22 17:43:18 2021 +0200
    
        feat: refactor a quickstart cloud example
    
    commit 1c8d62ebd377988342f95441856fad4902a911a0
    Author: geomodular <peter.szabo@h2o.ai>
    Date:   Tue Apr 13 17:42:34 2021 +0200
    
        feat: add a new quickstart example

---
+++ examples/quickstart-cloud-app/run.py
@@ -0,0 +1,109 @@
+"""
+Take a Titanic dataset, train a model and show a confusion matrix based on that model.
+"""
+
+from typing import List, Optional
+
+import datatable as dt
+from h2o_wave import main, app, Q, ui, handle_on, on
+from h2o_wave_ml import build_model, get_model
+from sklearn.metrics import confusion_matrix
+
+DATASET = './titanic.csv'
+TARGET_COLUMN = 'Survived'
+
+TEMPLATE = '''
+#
+| | | |
+|:-:|:-:|:-:|
+| | Survived | Not survived |
+| Survived | **{tp}** | {fp} (FP) |
+| Not survived | {fn} (FN) | **{tn}** |
+<br><br>
+'''
+
+
+def make_header(textbox_value: Optional[str] = '') -> List[ui.Component]:
+    return [
+        ui.text_xl('Confusion Matrix for Titanic'),
+        ui.textbox(name='project_id', label='Input the project id or the deployment endpoint url', value=textbox_value),
+        ui.buttons([
+            ui.button(name='train', label='Train new model'),
+            ui.button(name='load', label='Use model', primary=True),
+        ], justify='end')
+    ]
+
+
+def make_matrix(tn: float, fp: float, fn: float, tp: float, model_id: Optional[str]) -> List[ui.Component]:
+    return [
+        *make_header(model_id),
+        ui.text(TEMPLATE.format(tn=tn, fp=fp, fn=fn, tp=tp)),
+        ui.slider(name='slider', label='Threshold', min=0, max=1, step=0.01, value=0.5, trigger=True),
+    ]
+
+
+def make_message(type_: str, message: str) -> List[ui.Component]:
+    return [
+        *make_header(),
+        ui.message_bar(type=type_, text=message),
+    ]
+
+
+def make_loading() -> List[ui.Component]:
+    return [
+        ui.text_xl('Confusion Matrix for Titanic'),
+        ui.progress(label='Building a model'),
+    ]
+
+
+def compute_matrix(q: Q):
+    threshold = q.args.slider if 'slider' in q.args else 0.5
+    y_pred = [p[1] < threshold for p in q.client.prediction]
+    tn, fp, fn, tp = confusion_matrix(q.app.y_true, y_pred).ravel()
+    return dict(tn=tn, fp=fp, fn=fn, tp=tp)
+
+
+@on()
+async def train(q: Q):
+    q.page['matrix'].items = make_loading()
+    await q.page.save()
+
+    model = build_model(DATASET, target_column=TARGET_COLUMN, _dai_time=1, _dai_accuracy=1,
+                        _dai_interpretability=10, refresh_token=q.auth.refresh_token)
+
+    q.client.prediction = model.predict(file_path=DATASET)
+    q.page['matrix'].items = make_matrix(model_id=model.endpoint_url, **compute_matrix(q))
+
+
+@on()
+async def load(q: Q):
+    if q.args.project_id.startswith('http'):
+        model = get_model(endpoint_url=q.args.project_id)
+    else:
+        model = get_model(model_id=q.args.project_id, access_token=q.auth.access_token)
+
+    if model is None:
+        q.page['matrix'].items = make_message('error', 'The model does not exists.')
+    else:
+        q.client.prediction = model.predict(file_path=DATASET)
+        q.page['matrix'].items = make_matrix(model_id=q.args.project_id, **compute_matrix(q))
+
+
+@app('/')
+async def serve(q: Q):
+
+    if not q.app.initialized:
+        df = dt.fread(DATASET)
+        q.app.y_true = df[TARGET_COLUMN].to_list()[0]
+        q.app.initialized = True
+
+    if not q.client.initialized:
+        q.page['matrix'] = ui.form_card(box='1 1 3 6', items=make_message('blocked', 'No model loaded yet.'))
+        q.client.initialized = True
+
+    if not await handle_on(q):
+        if q.client.prediction:
+            q.page['matrix'].items[3].text.content = TEMPLATE.format(**compute_matrix(q))
+            q.page['matrix'].items[4].slider.value = q.args.slider
+
+    await q.page.save()
