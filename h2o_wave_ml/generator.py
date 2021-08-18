5099606267450208ffe772985c724696e0d52bc2
geomodular <peter.szabo@h2o.ai>
2021-08-18 17:59:54 +0200

    feat: add template for subst. #13

--- h2o_wave_ml/generator.py
+++ h2o_wave_ml/generator.py
@@ -1,6 +1,9 @@
-from pathlib import Path
-from typing import List, Dict, Any, Callable
 import argparse
+import fileinput
+from pathlib import Path
+import shutil
+from typing import List, Dict, Any, Callable
+
 import h2o
 
 # Column name, type and attributes.
@@ -41,10 +44,6 @@ def _make_new_id() -> Callable[[str], str]:
     return new_id
 
 
-def _pick_component(column_info: Dict[str, Any]):
-    ...
-
-
 def generate_utility_file(output_dir: str, column_info: List[Dict[str, Any]]):
     file_path = Path(output_dir).joinpath('utility.py')
     with open(file_path, 'w') as f:
@@ -59,15 +58,65 @@ def generate_utility_file(output_dir: str, column_info: List[Dict[str, Any]]):
                 key = c['name']
                 values = c['values']
                 f.write(f'\t\'{key}\': [ui.choice(str(item)) for item in {values}],\n')
-        f.write(f'}}\n')
+        f.write(f'}}\n\n')
 
         f.write(f'default_value = {{\n')
-        # for c in column_info:
-        #     key = c['name']
-        #     value = c['value']
-        #     value = f'\n{value}\n' if c['type'] == 'string' else value
-        #     f.write(f'\t\'{key}\': {value},\n')
+        for c in column_info:
+            key = c['name']
+            value = c['first_value']
+            value = f'\'{value}\'' if c['type'] in ('enum', 'string') else value
+            f.write(f'\t\'{key}\': {value},\n')
         f.write('}}\n\n')
+
+
+def prepare_templates(template_dir: str, output_dir: str, dataset_src_path: str, target: str,
+                      title: str, column_info: List[Dict[str, Any]]):
+
+    index_src_path = Path(template_dir).joinpath('from_dataset.py')
+    index_dst_path = Path(output_dir).joinpath('index.py')
+    dataset_filename = Path(dataset_src_path).name
+
+    shutil.copy2(index_src_path, index_dst_path)
+    shutil.copy2(dataset_src_path, output_dir)
+
+    t = lambda n: ' ' * n * 4  # Space generator.
+
+    defaults = ''
+    for c in column_info:
+        if c['name'] == target:
+            continue
+        id_ = c['id']
+        if c['type'] in ('int', 'real'):
+            defaults += f'{t(1)}{id_} = q.args.{id_} if q.args.{id_} else default_value[\'{id_}\']\n'
+        else:
+            defaults += f'{t(1)}{id_} = q.args.{id_} or default_value[\'{id_}\']\n'
+
+    names = [c['name'] for c in column_info if c['name'] != target]
+    names = ', '.join(names)
+    prediction = f'    input_data = [features, [{names}]]'
+
+    form_items = ''
+    for c in column_info:
+        if c['name'] == target:
+            continue
+        id_ = c['id']
+        name = c['name']
+        if c['type'] == 'enum':
+            form_items += f'{t(3)}ui.dropdown(name=\'{id_}\', label=\'{name}\', value={id_}, trigger= True, choices=choices[\'{id_}\']),\n'
+        elif c['type'] == 'int':
+            max_ = c['max']
+            min_ = c['min']
+            form_items += f'{t(3)}ui.slider(name=\'{id_}\', label=\'{name}\', min={min_}, max={max_}, step=1, value=float({id_}), trigger=True),\n'
+        else:
+            print('unknown type')
+
+    for line in fileinput.input([index_dst_path], inplace=True):
+        print(line.replace('__DATASET__', str(dataset_filename))
+                  .replace('__TARGET__', target)
+                  .replace('__DEFAULT_VALUES__', defaults)
+                  .replace('__PREDICTION__', prediction)
+                  .replace('__TITLE__', title)
+                  .replace('__FORM_ITEMS__', form_items), end='')
 
 
 def _get_type_specifics(frame: h2o.H2OFrame, type_: str) -> Dict[str, Any]:
@@ -76,19 +125,24 @@ def _get_type_specifics(frame: h2o.H2OFrame, type_: str) -> Dict[str, Any]:
         return {
             'nvalues': f.unique().nrows,
             'values': f.categories(),
+            'first_value': f[0, 0],
         }
     elif type_ in ('int', 'real'):
         return {
             'nvalues': f.unique().nrows,
             'max': f.max(),
             'min': f.min(),
+            'first_value': f[0, 0],
         }
     elif type_ == 'string':
-        return {}  # A placeholder.
+        return {
+            'first_value': f[0, 0],
+        }
     elif type_ == 'time':
         return {
             'max': f.max(),
             'min': f.min(),
+            'first_value': f[0, 0],
         }
 
     print(f'new type value {type_}')
@@ -109,7 +163,9 @@ if __name__ == '__main__':
     parser.add_argument('--h2o3-url', type=str, help='an H2O-3 instance to use (if any)')
     parser.add_argument('--from-dataset', type=str, help='a dataset to examine')
     parser.add_argument('--output-dir', type=str, help='a directory to save the generated project to', default='./')
+    parser.add_argument('--template-dir', type=str, help='a directory containing the templates for generator', default='./templates')
     parser.add_argument('--drop', type=str, metavar='column', nargs='*', help='a list of column names to skip')
+    parser.add_argument('--title', type=str, help='a title for the application', default='My app')
     args = parser.parse_args()
 
     if args.from_dataset is None:
@@ -122,4 +178,6 @@ if __name__ == '__main__':
     if args.drop is not None:
         cols = [col for col in cols if col["name"] not in args.drop]
 
+    prepare_templates(args.template_dir, args.output_dir, args.from_dataset, args.target_column,
+                      args.title, cols)
     generate_utility_file(args.output_dir, cols)
