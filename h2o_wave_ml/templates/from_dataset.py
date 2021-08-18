1a3b0b8aa60b9d5d3fb7e8bed74097020d8a2b9e
geomodular <peter.szabo@h2o.ai>
2021-08-18 18:00:36 +0200

    feat: add basic template #13

---
+++ h2o_wave_ml/templates/from_dataset.py
@@ -0,0 +1,42 @@
+from h2o_wave import main, app, Q, ui
+from h2o_wave_ml import build_model
+from utility import default_value, choices, features
+
+model = build_model(train_file_path='__DATASET__', target_column='__TARGET__')
+
+@app('/')
+async def serve(q: Q):
+
+    # Prepare feature values or use default ones
+__DEFAULT_VALUES__
+
+    # Prepare input data and do the predictions
+__PREDICTION__
+    score = model.predict(input_data)
+    score = score[0][0]
+
+    # Initialize page with a layout
+    if not q.client.initialized:
+        q.page['meta'] = ui.meta_card(box='', layouts=[
+            ui.layout(
+                breakpoint='xs',
+                width='576px',
+                zones=[ui.zone('body')],
+            )
+        ])
+        q.page['header'] = ui.header_card(
+            box='body',
+            title='__TITLE__',
+            subtitle='Cheers!',  # Delete?
+            icon='Wines',  # Delete?
+            icon_color='$red',
+        )
+__OUTPUT_CARD__
+        q.page['wine'] = ui.form_card(box='body', items=[
+__FORM_ITEMS__
+        ])
+        q.client.initialized = True
+    else:
+__SCORE_UPDATE__
+
+    await q.page.save()
