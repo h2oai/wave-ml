41687de213d3188b0ccfcda01e08880bfae3c6af
geomodular <peter.szabo@h2o.ai>
2021-08-20 17:14:50 +0200

    feat: clean up #13

--- h2o_wave_ml/generator.py
+++ h2o_wave_ml/generator.py
@@ -2,14 +2,28 @@ import argparse
 import fileinput
 from pathlib import Path
 import shutil
-from typing import List, Dict, Any, Callable
+from typing import List, Dict, Any, Callable, Tuple
 
 import h2o
 
-# Column name, type and attributes.
-# A proper name for the column respecting the redundancy (python name, name for gui, ...).
-# Picking a right component for column, input/output component.
-# Default row - a row without missing values.
+
+class _Buffer:
+
+    _INDENT_SPACES = 4
+
+    def __init__(self):
+        self._buffer = ''
+
+    @classmethod
+    def _indent(cls, n: int):
+        return ' ' * n * cls._INDENT_SPACES
+
+    def p(self, text: str = '', tab: int = 0):
+        self._buffer += self._indent(tab)
+        self._buffer += f'{text}\n'
+
+    def __str__(self):
+        return self._buffer
 
 
 def _make_new_id() -> Callable[[str], str]:
@@ -44,79 +58,130 @@ def _make_new_id() -> Callable[[str], str]:
     return new_id
 
 
-def generate_utility_file(output_dir: str, column_info: List[Dict[str, Any]]):
+def generate_utility_file(output_dir: str, target: str, column_info: List[Dict[str, Any]]):
+    b = _Buffer()
     file_path = Path(output_dir).joinpath('utility.py')
     with open(file_path, 'w') as f:
-        f.write('from h2o_wave import ui\n\n')
+        b.p('from h2o_wave import ui')
+        b.p()
 
-        features = [c['name'] for c in column_info]
-        f.write(f'features = {features}\n\n')
+        features = [c['name'] for c in column_info if c['name'] != target]
+        b.p(f'features = {features}')
+        b.p()
 
-        f.write(f'choices = {{\n')
+        b.p('choices = {')
         for c in column_info:
             if c['type'] == 'enum':
                 key = c['name']
                 values = c['values']
-                f.write(f'\t\'{key}\': [ui.choice(str(item)) for item in {values}],\n')
-        f.write(f'}}\n\n')
+                b.p(f'\'{key}\': [ui.choice(str(item)) for item in {values}],', tab=1)
+        b.p('}')
+        b.p()
 
-        f.write(f'default_value = {{\n')
+        b.p('default_value = {')
         for c in column_info:
             key = c['name']
             value = c['first_value']
             value = f'\'{value}\'' if c['type'] in ('enum', 'string') else value
-            f.write(f'\t\'{key}\': {value},\n')
-        f.write('}}\n\n')
+            b.p(f'\'{key}\': {value},', tab=1)
+        b.p('}')
+        b.p()
+
+        f.write(str(b))
 
 
-def prepare_templates(template_dir: str, output_dir: str, dataset_src_path: str, target: str,
-                      title: str, column_info: List[Dict[str, Any]]):
-
-    index_src_path = Path(template_dir).joinpath('from_dataset.py')
-    index_dst_path = Path(output_dir).joinpath('index.py')
-    dataset_filename = Path(dataset_src_path).name
-
-    shutil.copy2(index_src_path, index_dst_path)
-    shutil.copy2(dataset_src_path, output_dir)
-
-    t = lambda n: ' ' * n * 4  # Space generator.
-
-    defaults = ''
+def _prepare_defaults(target: str, column_info: List[Dict[str, Any]]) -> str:
+    b = _Buffer()
     for c in column_info:
         if c['name'] == target:
             continue
         id_ = c['id']
         if c['type'] in ('int', 'real'):
-            defaults += f'{t(1)}{id_} = q.args.{id_} if q.args.{id_} else default_value[\'{id_}\']\n'
+            b.p(f'{id_} = q.args.{id_} if q.args.{id_} else default_value[\'{id_}\']', tab=1)
         else:
-            defaults += f'{t(1)}{id_} = q.args.{id_} or default_value[\'{id_}\']\n'
+            b.p(f'{id_} = q.args.{id_} or default_value[\'{id_}\']', tab=1)
+    return str(b)
 
+
+def _prepare_prediction(target: str, column_info: List[Dict[str, Any]]) -> str:
     names = [c['name'] for c in column_info if c['name'] != target]
     names = ', '.join(names)
-    prediction = f'    input_data = [features, [{names}]]'
+    return f'    input_data = [features, [{names}]]\n'
 
-    form_items = ''
+
+def _prepade_form_items(target: str, column_info: List[Dict[str, Any]]) -> str:
+    # String - textbox w/ multiline for longer text based on threshold
+    # Int - slider (10 values) / spinbox (rest)
+    # Float - slider (more values but watch step) / spinbox
+    # Enum - choicegroup (10 values) / dropdown
+    # Time - datepicker
+    b = _Buffer()
     for c in column_info:
         if c['name'] == target:
             continue
         id_ = c['id']
         name = c['name']
         if c['type'] == 'enum':
-            form_items += f'{t(3)}ui.dropdown(name=\'{id_}\', label=\'{name}\', value={id_}, trigger= True, choices=choices[\'{id_}\']),\n'
+            b.p(f'ui.dropdown(name=\'{id_}\', label=\'{name}\', value={id_}, trigger=True, choices=choices[\'{id_}\']),', tab=3)
         elif c['type'] == 'int':
             max_ = c['max']
             min_ = c['min']
-            form_items += f'{t(3)}ui.slider(name=\'{id_}\', label=\'{name}\', min={min_}, max={max_}, step=1, value=float({id_}), trigger=True),\n'
+            b.p(f'ui.slider(name=\'{id_}\', label=\'{name}\', min={min_}, max={max_}, step=1, value=float({id_}), trigger=True),', tab=3)
         else:
             print('unknown type')
+    return str(b)
+
+
+def _prepare_output(target: str, column_info: List[Dict[str, Any]]) -> Tuple[str, str]:
+    # String, Enum - text, markdown, Info / Wide
+    # Int, Float - gauge,
+    # Time - convert to string (see clock component)
+    b = _Buffer()
+    u = _Buffer()
+    for c in column_info:
+        if c['name'] == target:
+            if c['type'] == 'int':
+                b.p('q.page[\'result\'] = ui.tall_gauge_stat_card(', tab=2)
+                b.p('box=ui.box(\'body\', height=\'180px\'),', tab=3)
+                b.p('value=str(score),', tab=3)
+                b.p('aux_value=\'{target}\',', tab=3)
+                b.p('title=\'Result\',', tab=3)
+                b.p('progress=0,', tab=3)
+                b.p(')', tab=2)
+                u.p('q.page[\'result\'].value = str(score)', tab=2)
+            else:
+                print('unknown type')
+            break
+    return str(b), str(u)
+
+
+def prepare_templates(template_dir: str, output_dir: str, dataset_src_path: str, target: str,
+                      title: str, column_info: List[Dict[str, Any]]):
+
+    makefile_src_path = Path(template_dir).joinpath('Makefile')
+    makefile_dst_path = Path(output_dir).joinpath('Makefile')
+    index_src_path = Path(template_dir).joinpath('from_dataset.py')
+    index_dst_path = Path(output_dir).joinpath('index.py')
+    dataset_filename = Path(dataset_src_path).name
+
+    shutil.copy2(makefile_src_path, makefile_dst_path)
+    shutil.copy2(index_src_path, index_dst_path)
+    shutil.copy2(dataset_src_path, output_dir)
+
+    defaults = _prepare_defaults(target, column_info)
+    prediction = _prepare_prediction(target, column_info)  # Choose better name.
+    form_items = _prepade_form_items(target, column_info)
+    output_card, score_update = _prepare_output(target, column_info)  # Choose better name.
 
     for line in fileinput.input([index_dst_path], inplace=True):
         print(line.replace('__DATASET__', str(dataset_filename))
                   .replace('__TARGET__', target)
-                  .replace('__DEFAULT_VALUES__', defaults)
-                  .replace('__PREDICTION__', prediction)
+                  .replace('__DEFAULT_VALUES__\n', defaults)
+                  .replace('__PREDICTION__\n', prediction)  # Choose better name.
                   .replace('__TITLE__', title)
-                  .replace('__FORM_ITEMS__', form_items), end='')
+                  .replace('__OUTPUT_CARD__\n', output_card)
+                  .replace('__FORM_ITEMS__\n', form_items)
+                  .replace('__SCORE_UPDATE__\n', score_update), end='')
 
 
 def _get_type_specifics(frame: h2o.H2OFrame, type_: str) -> Dict[str, Any]:
@@ -180,4 +245,4 @@ if __name__ == '__main__':
 
     prepare_templates(args.template_dir, args.output_dir, args.from_dataset, args.target_column,
                       args.title, cols)
-    generate_utility_file(args.output_dir, cols)
+    generate_utility_file(args.output_dir, args.target_column, cols)
